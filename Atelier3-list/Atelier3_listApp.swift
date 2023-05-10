3ce098eebac0066a1403533bad8c2df0490b9737
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-10 13:29:33 +0100

    Initial Commit

---
+++ Atelier3-list/Atelier3_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atelier3_listApp.swift
+//  Atelier3-list
+//
+//  Created by CHEBIHI FAYCAL on 10/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Atelier3_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
